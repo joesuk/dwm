31aa3dd34d53358f9096411d437e79652a2b75ab
joesuk <ybjosuk@gmail.com>
2024-09-15 16:30:44 -0400

    restartsig patch

--- dwm.c
+++ dwm.c
@@ -1877,6 +1877,9 @@ setup(void)
 	signal(SIGHUP, sighup);
 	signal(SIGTERM, sigterm);
 
+	signal(SIGHUP, sighup);
+	signal(SIGTERM, sigterm);
+
 	/* init screen */
 	screen = DefaultScreen(dpy);
 	sw = DisplayWidth(dpy, screen);
@@ -1974,19 +1977,19 @@ showhide(Client *c)
 	}
 }
 
-void
-sighup(int unused)
-{
-	Arg a = {.i = 1};
-	quit(&a);
-}
+/*void*/
+/*sighup(int unused)*/
+/*{*/
+	/*Arg a = {.i = 1};*/
+	/*quit(&a);*/
+/*}*/
 
-void
-sigterm(int unused)
-{
-	Arg a = {.i = 0};
-	quit(&a);
-}
+/*void*/
+/*sigterm(int unused)*/
+/*{*/
+	/*Arg a = {.i = 0};*/
+	/*quit(&a);*/
+/*}*/
 
 #ifndef __OpenBSD__
 void
